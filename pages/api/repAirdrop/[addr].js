90f5825a1721561e9e7eded143d97bc591e69b89
sirpy <hadarr@gmail.com>
2022-03-21 00:07:12 +0200

    fix: allow any origin

--- pages/api/repAirdrop/[addr].js
+++ pages/api/repAirdrop/[addr].js
@@ -8,7 +8,7 @@ import Cors from 'cors'
 
 // Initializing the cors middleware
 const cors = Cors({
-  origin: true //allow any
+  origin: '*' //allow any
 })
 
 // Helper method to wait for a middleware to execute before continuing
